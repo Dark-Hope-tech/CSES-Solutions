9734df39db326c715660064b05d92ac3507ecb35
Tabish Malik <76650437+Dark-Hope-tech@users.noreply.github.com>
2021-09-18 01:33:01 +0530

    Create Weird_Algorithm.cpp

---
+++ Introductory Problems/Weird_Algorithm.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+#define fast                 \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0);
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define f() for (ll i = 0; i < n; i++)
+#define all(v) v.begin(), v.end()
+#define pb push_back
+#define mp make_pair
+#define int ll
+#define vi vector<int>
+#define a6 1000001
+#define p push
+#define pii pair<int, int>
+#define um unordered_map<int, int>
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define pii pair<int, int>
+using namespace std;
+signed main()
+{
+    fast
+#ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin >> n;
+    while (n != 1)
+    {
+        cout << n << " ";
+        if (n % 2)
+        {
+            n = n * 3 + 1;
+        }
+        else
+            n /= 2;
+    }
+    cout << 1;
+}
